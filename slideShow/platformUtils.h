699e533011f222b069877ee3daaa1d757e6da83b
Administrator <Administrator@game>
2009-10-19 22:24:38 +0200

    - porting the project to  Microsoft Visual C++ 2008, Windows Mobile 6 Professional SDK, windows xp professional - adding copyright files and notices

--- slideShow/platformUtils.h
+++ slideShow/platformUtils.h
@@ -1,3 +1,36 @@
+/************************************************************
+ *
+ * Copyright 2009 Mohammed El-Afifi
+ * This file is part of slideShow.
+ *
+ * slideShow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * slideShow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with slideShow.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * file:         platformUtils.h
+ *
+ * function:
+ *
+ * description:  useful function and data declarations related to the windows
+ *				 platform
+ *
+ * author:       Mohammed Safwat (MS)
+ *
+ * environment:  Microsoft Visual C++ 2008, Windows Mobile 6 Professional SDK,
+ *				 windows xp profesional
+ *
+ * notes:        This is a private program.
+ *
+ ************************************************************/
 #include <list>
 #include <string>
 #include <WinDef.h>
